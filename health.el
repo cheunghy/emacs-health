e58b0b81505d192c6bcb555ea7fb440616a5d8b6
Zhang Kai Yu <yeannylam@gmail.com>
2015-10-01 21:29:25 +0800

    Add Cask

--- health.el
+++ health.el
@@ -1,4 +1,28 @@
-;; Life and health, family and oxygen.
+;;; health.el --- Track your working time
+
+;; Copyright (C) 2015 Zhang Kai Yu
+
+;; Author: Zhang Kai Yu <yeannylam@gmail.com>
+;; Keywords: health
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (defgroup health nil
   "Health timer."
@@ -92,3 +116,4 @@
                   (seconds-to-time (* 60 minutes)))))
 
 (provide 'health)
+;;; health.el ends here
